e9d6e2aa3da1d65af750c8bf394bdadd79030848
hieunmh <hieunm.pt@gmail.com>
2024-01-02 21:33:38 +0700

    add supabase and provider

---
+++ providers/UserProvider.tsx
@@ -0,0 +1,16 @@
+'use client';
+
+import { MyUserContextProvider } from '@/hooks/useUser';
+import React from 'react';
+
+interface UserProviderProps {
+  children: React.ReactNode;
+}
+
+export const UserProvider: React.FC<UserProviderProps> = ({ children }) => {
+  return (
+    <MyUserContextProvider>
+      {children}
+    </MyUserContextProvider>
+  )
+}
