dc32c1864f8d6b55d4a217d075a3632cd683b427
hieunmh <hieunm.pt@gmail.com>
2024-01-02 23:28:37 +0700

    auth modal

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,20 +1,44 @@
 'use client';
 
 import { useRouter } from 'next/navigation';
-import React from 'react';
+import React, { useState } from 'react';
 import { twMerge } from 'tailwind-merge';
 
 import { RxCaretLeft, RxCaretRight } from 'react-icons/rx';
 import { HiHome } from 'react-icons/hi';
-import { BiSearch } from 'react-icons/bi';
+import { BiLoaderCircle, BiSearch } from 'react-icons/bi';
+
 import { Button } from './Button';
+import { useAuthModal } from '@/hooks/useAuthModal';
+import { useUser } from '@/hooks/useUser';
+
+import { useSupabaseClient } from '@supabase/auth-helpers-react';
+import { FaUser } from 'react-icons/fa';
+import toast from 'react-hot-toast';
+
 
 export default function Header({ children, className } : { children: React.ReactNode, className: string }) {
 
   const router = useRouter();
+  const authModal = useAuthModal();
+
+  const supabaseClient = useSupabaseClient();
+
+  const { user } = useUser();
+
+  let [isLoading, setIsloading] = useState<boolean>(false);
 
   const handleLogout = async () => {
+    setIsloading(true);
+    const { error } = await supabaseClient.auth.signOut();
+    setIsloading(false);
+    router.refresh();
 
+    if (error) {
+      toast.error(error.message);
+    } else {
+      toast.success('Logged out!')
+    }
   }
 
   return (
@@ -43,17 +67,40 @@ export default function Header({ children, className } : { children: React.React
         </div>
 
         <div className='flex justify-between items-center gap-x-4'>
-          <div>
-            <Button onClick={() => {}} className='bg-transparent text-neutral-300 font-medium'>
-              Sign Up
-            </Button>
-          </div>
+          {user ? (
+            <div className='flex gap-x-4 items-center'>
+              <Button onClick={handleLogout} className='bg-white px-6 py-2 flex items-center'>
+                {isLoading ? (
+                  <div className='h-[25px] w-[50px] flex items-center justify-center'>
+                    <BiLoaderCircle size={20} className='animate-spin' />
+                  </div>
+                ) : (
+                  <div className='h-[25px] w-[50px] flex items-center justify-center'>
+                    Logout
+                  </div>
+                )}
 
-          <div>
-            <Button onClick={() => {}} className='bg-white px-6 py-2'>
-              Login
-            </Button>
-          </div>
+              </Button>
+
+              <Button onClick={() => router.push('/account')} className='bg-white'>
+                <FaUser />
+              </Button>
+            </div>
+          ) : (
+            <>
+              <div>
+                <Button onClick={authModal.onOpen} className='bg-transparent text-neutral-300'>
+                  Sign Up
+                </Button>
+              </div>
+
+              <div>
+                <Button onClick={authModal.onOpen} className='bg-white px-6 py-2'>
+                  Login
+                </Button>
+              </div>
+            </>
+          )}
         </div>
       </div>
       {children}
